5fbaba2be07f2d8f622e85ce5d07a230cfe7a657
Costa <costakob@gmail.com>
2020-04-23 21:46:03 +0300

    now its realy ready

--- yoga/dist/main.js
+++ yoga/dist/main.js
@@ -238,6 +238,14 @@ function modalWindow() {
         document.body.style.overflow = 'hidden';
     });
 
+    descriptionBtn.forEach(function(item) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
     close.addEventListener('click', function () {
         overlay.style.display = 'none';
         more.classList.remove('more-splash');
